740a8e9d3f416d4d6b9e06d5d3a15d398f721292
MARCELO GARCIA MURO <frmgarci@pichincha.com>
2025-09-10 22:04:01 -0500

    Devops Challenge Fullstack

---
+++ terraform/main.tf
@@ -0,0 +1,160 @@
+# App bucket
+resource "aws_s3_bucket" "app" {
+  bucket = "${var.bucket_name}-${var.environment}-devops-app"
+}
+
+resource "aws_s3_bucket_public_access_block" "app_public_access_block" {
+  bucket = aws_s3_bucket.app.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Logs bucket
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.bucket_name}-${var.environment}-devops-logs"
+}
+
+resource "aws_s3_bucket_ownership_controls" "logs_ownership" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.logs.id
+  acl    = "log-delivery-write"
+
+  depends_on = [aws_s3_bucket_ownership_controls.logs_ownership]
+}
+
+# Cloudfront
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "oac-s3-private-${var.environment}"
+  description                       = "OAC for S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.app.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "s3-origin-${var.environment}"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = aws_s3_bucket.logs.bucket_domain_name
+    prefix          = "cloudfront-logs/"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin-${var.environment}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# s3 bucket policies
+# app
+data "aws_iam_policy_document" "app_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadOnly"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.app.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["${aws_cloudfront_distribution.s3_distribution.arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "app_policy" {
+  bucket = aws_s3_bucket.app.id
+  policy = data.aws_iam_policy_document.app_policy.json
+}
+
+#Logs
+data "aws_iam_policy_document" "logs_policy" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalToWriteLogs"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.logs.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["${aws_cloudfront_distribution.s3_distribution.arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs_policy" {
+  bucket = aws_s3_bucket.logs.id
+  policy = data.aws_iam_policy_document.logs_policy.json
+}
+
+# Null resource
+resource "null_resource" "upload_build_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ../build s3://${aws_s3_bucket.app.bucket}"
+  }
+
+  depends_on = [aws_s3_bucket.app]
+}
